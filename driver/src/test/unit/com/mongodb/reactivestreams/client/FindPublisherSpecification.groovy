3020d9932ff1450f1d52d8dba85e3ff21f9b2337
Ross Lawley <ross.lawley@gmail.com>
2015-03-09 09:11:55 +0000

    Updates to latest 3.0 snapshot

--- driver/src/test/unit/com/mongodb/reactivestreams/client/FindPublisherSpecification.groovy
+++ driver/src/test/unit/com/mongodb/reactivestreams/client/FindPublisherSpecification.groovy
@@ -39,7 +39,7 @@ import static com.mongodb.ReadPreference.primary
 import static com.mongodb.ReadPreference.secondary
 import static com.mongodb.reactivestreams.client.CustomMatchers.isTheSameAs
 import static java.util.concurrent.TimeUnit.MILLISECONDS
-import static org.bson.codecs.configuration.CodecRegistryHelper.fromProviders
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders
 import static spock.util.matcher.HamcrestSupport.expect
 
 class FindPublisherSpecification extends Specification {
--- driver/src/test/unit/com/mongodb/reactivestreams/client/ListCollectionsPublisherSpecification.groovy
+++ driver/src/test/unit/com/mongodb/reactivestreams/client/ListCollectionsPublisherSpecification.groovy
@@ -32,7 +32,7 @@ import spock.lang.Specification
 import static com.mongodb.ReadPreference.secondary
 import static com.mongodb.reactivestreams.client.CustomMatchers.isTheSameAs
 import static java.util.concurrent.TimeUnit.MILLISECONDS
-import static org.bson.codecs.configuration.CodecRegistryHelper.fromProviders
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders
 import static spock.util.matcher.HamcrestSupport.expect
 
 class ListCollectionsPublisherSpecification extends Specification {
