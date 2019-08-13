34ab22c8452f8862a941de64631bfe20787aed34
Ross Lawley <ross.lawley@gmail.com>
2019-08-13 17:20:27 +0100

    Docs: 1.12

--- 1.12/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.mongodb.reactivestreams.client"},{"l":"com.mongodb.reactivestreams.client.gridfs"},{"l":"com.mongodb.reactivestreams.client.gridfs.helpers"},{"l":"com.mongodb.reactivestreams.client.vault"}]
